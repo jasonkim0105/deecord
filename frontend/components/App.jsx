2886f3581ea06d3c5fadf4b6e91a24fc0c9c9cb9
Jason <k.jason0105@gmail.com>
2021-09-24 15:54:28 -0700

    finished setting up react

---
+++ frontend/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route } from "react-router";
+// import { AuthRoute } from "../util/route_util";
+
+const App = () => (
+  <div>
+    <header>
+      <h1>My super awesome app is on it's way</h1>
+    </header>
+
+  </div>
+);
+
+export default App;

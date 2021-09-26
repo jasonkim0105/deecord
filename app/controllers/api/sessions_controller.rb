056b7406323ae26a218881370521adfb966bd79d
Jason <k.jason0105@gmail.com>
2021-09-26 15:19:07 -0700

    heroku

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,23 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.nil?
+      render json: ['Invalid username or password'], status: 401
+    else
+      login!(@user)
+      render 'api/users/show'
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout!
+      render json: {}
+    else
+      render json: ["Not Logged In"], status: 404
+    end
+  end
+
+
 end

a4bc4421ff726142ea8f6ba92d0712a886b30782
Jason <k.jason0105@gmail.com>
2021-09-26 22:53:07 -0700

    more controllers edits

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::SessionsController < ApplicationController
   def create
-    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    @user = User.find_by_credential(params[:user][:username], params[:user][:password])
     if @user.nil?
       render json: ['Invalid username or password'], status: 401
     else
--- frontend/deecord.jsx
+++ frontend/deecord.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
+import {login, logout, signup } from './util/session_api_util';
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  // window.login = login;
-  // window.logout = logout;
-  // window.signup = signup;
 
   const root = document.getElementById('root');
   const store = configureStore();
+
+  // TESTING START
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
+  // TESTING END
 
   ReactDOM.render(<Root store={store} />, root);
 })

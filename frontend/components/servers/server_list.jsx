01b07e41ce5b3f29b50b380ef2e472ed5481c0b7
Jason <k.jason0105@gmail.com>
2021-10-07 11:32:46 -0700

    server-list-fixinging up

--- frontend/components/servers/server_list.jsx
+++ frontend/components/servers/server_list.jsx
@@ -1,39 +1,56 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const ServerList = ( { server }) => {
-  let serverName;
+// const ServerList = ( { server }) => {
+//   let serverName;
 
-  if (server) {
-    if (server.name.length > 20) {
-      serverName = server.name.slice(0, 21) + '...';
-    } else {
-      serverName = server.name;
-    }
-  }
+//   if (server) {
+//     if (server.name.length > 20) {
+//       serverName = server.name.slice(0, 21) + '...';
+//     } else {
+//       serverName = server.name;
+//     }
+//   }
 
-  return(
-    <li className='servers-list-short-name'>
-      <NavLink
-        to={`/channels/${server.id}`}
-        className='servers-nav-link'>
-        {server.name[0]}
-      </NavLink>
+//   return(
+//     <li className='servers-list-short-name'>
+//       <NavLink
+//         to={`/channels/${server.id}`}
+//         className='servers-nav-link'>
+//         {server.name[0]}
+//       </NavLink>
 
-      {/* <div className='servers-list-names'>{serverName}</div> */}
-    </li>
-  );
-};
+//       {/* <div className='servers-list-names'>{serverName}</div> */}
+//     </li>
+//   );
+// };
 
 
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 
-const mapStateToProps = state => {
+// const mapStateToProps = state => {
 
-};
+// };
 
-const mapDispatchToProps = dispatch => {
+// const mapDispatchToProps = dispatch => {
 
-};
+// };
 
-export default connect(null, null)(ServerList);
+// export default connect(null, null)(ServerList);
+ class ServerList extends React.Component {
+   constructor(props) {
+     super(props);
+   }
+ }
+
+ import {connect} from 'react-redux';
+
+ const mapStateToProps = (state, ownProps) => ({
+
+})
+
+const mapDispatchToProps = dispatch => ({ fetchChannels: channels => dispatch(fetchChannels(channels)),
+  openModal: modal => dispatch(openModal(modal)), // fetchServerMembers: serverId => dispatch(fetchServerMembers(serverId))
+});
+
+export default connect(null, mapDispatchToProps)(ServerList);

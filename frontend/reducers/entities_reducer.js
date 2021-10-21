f2c6f82115653bf1b99a72e662540b3a3c8e9748
Jason <k.jason0105@gmail.com>
2021-10-20 23:29:52 -0700

    for heroku

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import {combineReducers} from 'redux';
 import UsersReducer from './users_reducer';
 import ServersReducer from './servers_reducer';
 import ChannelsReducer from './channels_reducer';
+import MessagesReducer from './messages_reducers';
 
 const EntitiesReducer = combineReducers({
     users: UsersReducer,
     servers: ServersReducer,
-    channels: ChannelsReducer
+    channels: ChannelsReducer,
+    messages: MessagesReducer
 })
 
 export default EntitiesReducer;

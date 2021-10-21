c45606bff4e0e28ed1bb43ebbadfecbad108769f
Jason <k.jason0105@gmail.com>
2021-10-20 23:34:27 -0700

    for heroku push

--- frontend/reducers/messages_reducers.js
+++ frontend/reducers/messages_reducers.js
@@ -1,13 +1,13 @@
-import { RECEIVE_MESSAGE, RECEIVE_MESSAGES, CLEAR_MESSAGES} from '../actions/message_actions';
+// import { RECEIVE_MESSAGE, RECEIVE_MESSAGES, CLEAR_MESSAGES} from '../actions/message_actions';
 
-const MessagesReducer = (state = {}, action) => {
-  Object.freeze(state);
-  switch(action.type) {
-    case RECEIVE_MESSAGES:
-      return Object.assign({}, action.messages)
-    case RECEIVE_MESSAGE:
-      return Object.assign({}, state, { [action.message.id]: action.message })
-    case CLEAR_MESSAGES:
-      return {}
-  }
-}
+// const MessagesReducer = (state = {}, action) => {
+//   Object.freeze(state);
+//   switch(action.type) {
+//     case RECEIVE_MESSAGES:
+//       return Object.assign({}, action.messages)
+//     case RECEIVE_MESSAGE:
+//       return Object.assign({}, state, { [action.message.id]: action.message })
+//     case CLEAR_MESSAGES:
+//       return {}
+//   }
+// }
